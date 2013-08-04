812ccda67481963a56bba7b015ced17f3080b795
Steffen Itterheim <steffen@learn-cocos2d.com>
2013-08-04 21:32:40 +0200

    doc

--- KoboldKit/PlatformerDemo/Resources/Config/behaviorTemplates.lua
+++ KoboldKit/PlatformerDemo/Resources/Config/behaviorTemplates.lua
@@ -13,7 +13,7 @@ local behaviorTemplates =
 		{
 			pathName = "<missing path>",	-- name of path object
 			moveSpeed = 10,					-- movement speed in points per frame
-			relativeToObject = NO,			-- if YES, object will follow path as if path were at object's position
+			relativeToObject = NO,			-- if YES, object will follow path but with object's current position as offset
 			orientToPath = NO,				-- if YES, object will rotate in direction of current path segment
 			initialWaitDuration = 0,		-- start follow immediately (time in seconds)
 			repeatCount = 0,					-- 0 == repeat indefinitely, for polylines 1 == move to end, 2 == move to end and back to start
