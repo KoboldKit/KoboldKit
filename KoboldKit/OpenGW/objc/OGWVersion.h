4d3e9ac4474baacf7ee74bbc089a0ca2f53e11ff
Steffen Itterheim <steffen@learn-cocos2d.com>
2013-09-18 20:10:37 +0200

    add ogw version

---
+++ KoboldKit/OpenGW/objc/OGWVersion.h
@@ -0,0 +1,14 @@
+//
+//  OGWVersion.h
+//  OpenGW
+//
+//  Created by Steffen Itterheim on 18/09/13.
+//  Copyright (c) 2013 Steffen Itterheim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/** @file KKProVersion.h */
+
+/** Returns the Kobold Kit Pro version as string. */
+NSString* OGWVersion();
