bb881654bd9739adf203b9535ec9d0339c2468b3
Steffen Itterheim <steffen@learn-cocos2d.com>
2013-10-14 15:47:06 +0200

    kk free: fix audio session crash on background

--- KoboldKit/KoboldKitFree/Framework/App/KKAppDelegate.m
+++ KoboldKit/KoboldKitFree/Framework/App/KKAppDelegate.m
@@ -6,6 +6,7 @@
 
 
 #import "KKAppDelegate.h"
+#import <AVFoundation/AVFoundation.h>
 
 @implementation KKAppDelegate
 
@@ -20,17 +21,24 @@
 {
 	// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
 	// Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+
+	// prevent audio crash
+	[[AVAudioSession sharedInstance] setActive:NO error:nil];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
 	// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
 	// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+
+	// prevent audio crash
+	[[AVAudioSession sharedInstance] setActive:NO error:nil];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
 	// Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+	[[AVAudioSession sharedInstance] setActive:YES error:nil];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
